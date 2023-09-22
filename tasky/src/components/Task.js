7d4d936ec7134569f908a040bbd75fe11e4713b5
lauradhg18 <lauradhg18@uma.es>
2023-09-22 17:16:04 +0200

    Completed Week 1 Lab

---
+++ tasky/src/components/Task.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Task = (props) => {
+    
+    return (
+      <div className= "card">
+        <p className="title">{props.title}</p>
+        <p>Due: {props.deadline}</p>
+        <p className="description">{props.description}</p>
+    </div>
+        
+    )
+}
+/*function Task(){
+    
+    return (
+        <p>This is a task!</p>
+    )
+} */
+export default Task;
